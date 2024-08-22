f7d703b85b6f9b45e196c40b1ea925b29feb39c8
theminhdayma <147359915+theminhdayma@users.noreply.github.com>
2024-08-22 11:51:20 +0700

    Add hackathon01

---
+++ PTIT_Hackathon_KS_NguyenTheMinh/hackathon01/app/page.tsx
@@ -0,0 +1,19 @@
+import Employee from "@/employees/page";
+import EmployeeTable from "../components/EmployeeTable";
+import EmployeeForm from "../components/EmployeeForm";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-4 py-6">
+      <h1 className="text-2xl font-bold mb-6">Quản lý nhân viên</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="col-span-2">
+          <EmployeeTable />
+        </div>
+        <div>
+          <EmployeeForm />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ PTIT_Hackathon_KS_NguyenTheMinh/hackathon01/employees/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import EmployeeTable from "../components/EmployeeTable";
+import EmployeeForm from "../components/EmployeeForm";
+
+export default function Employee() {
+  return (
+    <div className="container mx-auto px-4 py-6">
+      <h1 className="text-2xl font-bold mb-6">Quản lý nhân viên</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="col-span-2">
+          <EmployeeTable />
+        </div>
+        <div>
+          <EmployeeForm />
+        </div>
+      </div>
+    </div>
+  );
+}
